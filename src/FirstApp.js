9d92dcdb31221f09ec6d5760d6532e80f6c61997
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-21 19:57:38 -0300

    component

---
+++ src/FirstApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// Statefull Class Component (SCC) -> Class Component(CC)
+// Stateless Functional Component (SFC) -> Functional Component(FC)
+
+const FirstApp = () => {
+  return <h1>Hello World</h1>;
+}
+
+export default FirstApp;
