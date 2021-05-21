5cd4c4f2618bdfaa7595c7e5a6d6558d0b2145e7
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-21 20:15:57 -0300

    fragments

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,9 +1,34 @@
 import React from 'react';
+// import React, { Fragment } from 'react';
 // Statefull Class Component (SCC) -> Class Component(CC)
 // Stateless Functional Component (SFC) -> Functional Component(FC)
 
 const FirstApp = () => {
-  return <h1>Hello World</h1>;
+// javascript solo puede retornar un objeto a la vez
+// creamos un div solo para contener h1 y p
+  // return (
+  //   <div>
+  //     <h1>Hello World</h1>
+  //     <p>My First Page</p>
+  //   </div>
+  // );
+  
+  // podemos usar un Fragment (Higher order component)
+  // componente que recibe otros componentes hijos
+  // return(
+  //   <Fragment>
+  //     <h1>Hello World</h1>
+  //     <p>My First Page</p>
+  //   </Fragment>
+  // )
+
+  // podemos usar la forma corta para usar fragments
+  return (
+    < >
+      <h1>Hello World</h1>
+      <p>My First Page</p>
+    </ >
+  )
 }
 
 export default FirstApp;
