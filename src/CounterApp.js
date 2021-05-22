3fa540b7d7d0604b8f1140ebc82bd1d6f8bb28fb
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-21 22:00:18 -0300

    event click

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,11 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+  // event in react
+  // https://es.reactjs.org/docs/events.html
 
+  // handleAdd
+    const handleAdd = e => console.log(e);
   return(
     <>
       <h1>CounterApp</h1>
       <p>{value}</p>
+      {/*<button onClick={ () => {console.log('+1')} }>+1</button>*/}
+      {/*<button onClick={ (event) => {console.log(event)} }>+1</button>*/}
+      {/*<button onClick={ event => handleAdd(event)}>+1</button>*/}
+      <button onClick={ handleAdd }>+1</button>
     </>
   );
 }
